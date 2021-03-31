51d37619a8286e76621855003e29b75832fe548f
unknown <bastian.santana@fusiona.cl>
2021-03-30 22:23:42 -0300

    ecisystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       script: "serve",
       env: {
           PM2_SERVE_PATH: './dist',
-          PM2_SERVE_PORT: 8080,
+          PM2_SERVE_PORT: 8082,
           PM2_SERVE_SPA: 'true',
           PM2_SERVE_HOMEPAGE: '/index.html'
       }
