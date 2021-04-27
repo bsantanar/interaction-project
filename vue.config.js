fd3d53f781657bb2351751844dc7c606bee403c5
Bastián Santana <bsantana17@gmail.com>
2021-04-26 22:39:39 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 process.env.VUE_APP_API_URL = "http://localhost:3000/api";
-process.env.VUE_APP_PROJECT_ID = "600c3c79245fa93878cf4955";
-// process.env.VUE_APP_PROJECT_ID = "ID_PROJECT";
+// process.env.VUE_APP_PROJECT_ID = "600c3c79245fa93878cf4955";
+process.env.VUE_APP_PROJECT_ID = "ID_PROJECT";
 
 module.exports = {
   chainWebpack: config => {
